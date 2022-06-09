ed8c754fe920b6daf45bfdce16a951b0105241b9
rnk0085 <rinawkmbi4k8@gmail.com>
2022-06-09 14:46:49 +0900

    feat: OutlinedTextFieldを追加

--- app/src/main/java/com/rnk0085/android/jetpackcomposepractice/FirstSceen.kt
+++ app/src/main/java/com/rnk0085/android/jetpackcomposepractice/FirstSceen.kt
@@ -3,10 +3,15 @@ package com.rnk0085.android.jetpackcomposepractice
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -26,10 +31,19 @@ fun FirstScreen(
         }
     ) {
         Column(
-            modifier = Modifier.padding(24.dp)
+            modifier = Modifier.padding(8.dp)
         ) {
-            Text(text = "最初の画面")
-            Button(onClick = onNextButtonClick) {
+            Text(text = "最初の画面", modifier = Modifier.padding(bottom = 8.dp))
+
+            var text by remember { mutableStateOf("") }
+
+            OutlinedTextField(
+                value = text,
+                onValueChange = { text = it },
+                label = { Text(text = "name") }
+            )
+
+            Button(onClick = onNextButtonClick, modifier = Modifier.padding(top = 8.dp)) {
                 Text(text = "Next")
             }
         }
