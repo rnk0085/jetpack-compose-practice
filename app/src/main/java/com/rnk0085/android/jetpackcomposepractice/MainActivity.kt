694877748b68dfb7669ef1ab5a6e3420b869aae9
rnk0085 <rinawkmbi4k8@gmail.com>
2022-06-13 20:36:26 +0900

    feat: 4つの画面を作成

--- app/src/main/java/com/rnk0085/android/jetpackcomposepractice/MainActivity.kt
+++ app/src/main/java/com/rnk0085/android/jetpackcomposepractice/MainActivity.kt
@@ -3,11 +3,15 @@ package com.rnk0085.android.jetpackcomposepractice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.rnk0085.android.jetpackcomposepractice.ui.theme.JetpackComposePracticeTheme
@@ -17,10 +21,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposePracticeTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    First()
                 }
             }
         }
@@ -28,14 +31,54 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Screen(name: String, onButtonClick: () -> Unit) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = name)
+        Button(onClick = onButtonClick) {
+            Text(text = "Next")
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun First() {
+    Screen(
+        name = "First",
+        onButtonClick = {}
+    )
+}
+
+@Composable
+fun Second() {
+    Screen(
+        name = "Second",
+        onButtonClick = {}
+    )
+}
+
+@Composable
+fun Third() {
+    Screen(
+        name = "Third",
+        onButtonClick = {}
+    )
+}
+
+@Composable
+fun Fourth() {
+    Screen(
+        name = "Fourth",
+        onButtonClick = {}
+    )
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 640)
 @Composable
 fun DefaultPreview() {
     JetpackComposePracticeTheme {
-        Greeting("Android")
+        First()
     }
 }
