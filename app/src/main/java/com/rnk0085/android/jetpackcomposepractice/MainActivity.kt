f5a9a2cea366a56f4731170e57edb5c404babdd9
rnk0085 <rinawkmbi4k8@gmail.com>
2022-06-13 21:03:29 +0900

    feat: NavHostを作成

--- app/src/main/java/com/rnk0085/android/jetpackcomposepractice/MainActivity.kt
+++ app/src/main/java/com/rnk0085/android/jetpackcomposepractice/MainActivity.kt
@@ -14,6 +14,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.rnk0085.android.jetpackcomposepractice.ui.theme.JetpackComposePracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,18 +25,64 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposePracticeTheme {
-                Surface(modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background) {
-                    First()
-                }
+                val navController = rememberNavController()
+                MyNavHost(navController = navController)
             }
         }
     }
 }
 
 @Composable
+fun MyNavHost(
+    navController: NavHostController
+) {
+    NavHost(
+        navController = navController,
+        startDestination = Routes.First.route
+    ) {
+        composable(Routes.First.route) {
+            First(
+                onButtonClick = {
+                    navController.navigate(Routes.Second.route)
+                }
+            )
+        }
+
+        composable(Routes.Second.route) {
+            Second(
+                onButtonClick = {
+                    navController.navigate(Routes.Third.route)
+                }
+            )
+        }
+
+        composable(Routes.Third.route) {
+            Third(
+                onButtonClick = {
+                    navController.navigate(Routes.Fourth.route)
+                }
+            )
+        }
+
+        composable(Routes.Fourth.route) {
+            Fourth(
+                onButtonClick = {}
+            )
+        }
+    }
+}
+
+sealed class Routes(val route: String) {
+    object First : Routes("first")
+    object Second : Routes("second")
+    object Third : Routes("third")
+    object Fourth : Routes("fourth")
+}
+
+@Composable
 fun Screen(name: String, onButtonClick: () -> Unit) {
     Column(
+        modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
@@ -44,41 +94,57 @@ fun Screen(name: String, onButtonClick: () -> Unit) {
 }
 
 @Composable
-fun First() {
+fun First(
+    onButtonClick: () -> Unit
+) {
     Screen(
         name = "First",
-        onButtonClick = {}
+        onButtonClick = onButtonClick
     )
 }
 
 @Composable
-fun Second() {
+fun Second(
+    onButtonClick: () -> Unit
+) {
     Screen(
         name = "Second",
-        onButtonClick = {}
+        onButtonClick = onButtonClick
     )
 }
 
 @Composable
-fun Third() {
+fun Third(
+    onButtonClick: () -> Unit
+) {
     Screen(
         name = "Third",
-        onButtonClick = {}
+        onButtonClick = onButtonClick
     )
 }
 
 @Composable
-fun Fourth() {
-    Screen(
-        name = "Fourth",
-        onButtonClick = {}
-    )
+fun Fourth(
+    onButtonClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Fourth")
+        Button(onClick = onButtonClick) {
+            Text(text = "Back")
+        }
+    }
 }
 
 @Preview(showBackground = true, widthDp = 320, heightDp = 640)
 @Composable
 fun DefaultPreview() {
     JetpackComposePracticeTheme {
-        First()
+        First(
+            onButtonClick = {}
+        )
     }
 }
