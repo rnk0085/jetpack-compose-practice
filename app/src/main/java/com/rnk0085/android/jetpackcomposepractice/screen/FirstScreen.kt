7e9497fb354bb202d586eb92454e85e7aa936af0
rnk0085 <rinawkmbi4k8@gmail.com>
2022-06-20 20:34:51 +0900

    chore: 4画面を用意

---
+++ app/src/main/java/com/rnk0085/android/jetpackcomposepractice/screen/FirstScreen.kt
@@ -0,0 +1,9 @@
+package com.rnk0085.android.jetpackcomposepractice.screen
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FirstScreen() {
+    Text(text = "FirstScreen")
+}
---
+++ app/src/main/java/com/rnk0085/android/jetpackcomposepractice/screen/FourthScreen.kt
@@ -0,0 +1,9 @@
+package com.rnk0085.android.jetpackcomposepractice.screen
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FourthScreen() {
+    Text(text = "FourthScreen")
+}
