799d37f93f35591eae2def0965146a9ab332c2a5
rnk0085 <rinawkmbi4k8@gmail.com>
2022-06-20 21:25:52 +0900

    refactor: 画面上の文字などを修正

--- app/src/main/java/com/rnk0085/android/jetpackcomposepractice/screen/FirstScreen.kt
+++ app/src/main/java/com/rnk0085/android/jetpackcomposepractice/screen/FirstScreen.kt
@@ -1,9 +1,20 @@
 package com.rnk0085.android.jetpackcomposepractice.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
 fun FirstScreen() {
-    Text(text = "FirstScreen")
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Home")
+    }
 }
