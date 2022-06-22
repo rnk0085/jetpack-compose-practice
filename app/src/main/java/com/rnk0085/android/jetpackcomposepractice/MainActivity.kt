df2eda4f94ef0a08bc3dd766e2727cecf9b18b86
rnk0085 <rinawkmbi4k8@gmail.com>
2022-06-22 14:51:05 +0900

    feat: TextFieldを作成

--- app/src/main/java/com/rnk0085/android/jetpackcomposepractice/MainActivity.kt
+++ app/src/main/java/com/rnk0085/android/jetpackcomposepractice/MainActivity.kt
@@ -3,16 +3,20 @@ package com.rnk0085.android.jetpackcomposepractice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.rnk0085.android.jetpackcomposepractice.ui.theme.JetpackComposePracticeTheme
@@ -30,10 +34,23 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp() {
+    // TODO: ViewModelを使用
     var openDialog by remember { mutableStateOf(false) }
+    var text by remember { mutableStateOf("") }
 
-    Button(onClick = { openDialog = true }) {
-        Text(text = "SHOW DIALOG")
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        // TODO: 条件を満たさないものを独自にException定義
+        Text(text = "0~9の数字を入力してください")
+
+        TextField(value = text, onValueChange = { text = it })
+
+        Button(onClick = { openDialog = true }) {
+            Text(text = "決定")
+        }
     }
 
     if (openDialog) {
